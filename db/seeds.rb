cb1379caf08fa91ede58cc54cfbe886528400f78
Peter Gorgone <peter@peltronic.com>
2019-01-08 09:26:47 -0800

    add seedfile for formcomponents

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Formcomponent.create!([
+  { fcname: "FooBar" },
+  { fcname: "Personal Info" },
+  { fcname: "Billing Address" },
+  { fcname: "Qualifications" },
+  { fcname: "Hobbies" },
+  { fcname: "Skills" },
+  { fcname: "Education" }
+])
+ 
+p "Done"
